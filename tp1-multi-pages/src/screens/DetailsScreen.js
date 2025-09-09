f66c840ffce2a9984b33093cb2f1bd344db992c4
Papemomath <papemomathdiba@gmail.com>
2025-09-09 13:48:37 -0400

    design de la page detail

--- tp1-multi-pages/src/screens/DetailsScreen.js
+++ tp1-multi-pages/src/screens/DetailsScreen.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native';
+
+import german from "../../assets/dog/german.png";
+import chihuahua from "../../assets/dog/chihuahua.png";
+import poodle from "../../assets/dog/poodle.png";
 
 export default function DetailsScreen() {
+
+  const [chien, setChien] = useState("Votre chien");
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Écran Détails</Text>
-      <Text>Voici un écran poussé via la navigation de type Stack.</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>{chien}</Text>
+
+        <View style={styles.containerGrid}> 
+          <Image style={styles.image} source={german} />
+
+          <TouchableOpacity onPress={() => setChien("German Shepherds")} style={styles.button}>
+            <Text style={styles.text} TouchableOpacity>German</Text>
+          </TouchableOpacity>
+
+          <Image style={styles.image} source={chihuahua} />
+
+            <TouchableOpacity onPress={() => setChien("Chihuahuas")} style={styles.button}>
+              <Text style={styles.text}>Chihuahuas</Text>
+            </TouchableOpacity>
+
+          <Image style={styles.image} source={poodle} />
+
+            <TouchableOpacity onPress={() => setChien("Poodles")} style={styles.button}>
+              <Text style={styles.text}>Poodles</Text>
+            </TouchableOpacity>
+        </View>
+
+      </View>
+    </ScrollView>
   );
 }
 
@@ -22,4 +51,22 @@ const styles = StyleSheet.create({
     fontWeight: '600', 
     marginBottom: 8 
   },
+  image: {
+    width:200,
+    height:200,
+    position:"relative",
+    right:75
+  },
+  button: {
+    borderRadius:10, 
+    borderWidth:2,
+    position: "relative",
+    left: 150,
+    width: 100,
+    bottom: 100,
+    padding: 10
+  }, 
+  text: {
+    textAlign: "center"
+  }
 });
