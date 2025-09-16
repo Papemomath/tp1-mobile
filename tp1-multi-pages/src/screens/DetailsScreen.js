1e1905c9eb02f8f5f2ae23429a9764191aee698c
yanis26x <yanis.djenadi@hotmail.com>
2025-09-16 11:11:43 -0400

    ajout de ma fonctionnaliter qui est trop cool on va se mentir

--- tp1-multi-pages/src/screens/DetailsScreen.js
+++ tp1-multi-pages/src/screens/DetailsScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView} from 'react-native';
 
 import german from "../../assets/dog/german.png";
@@ -8,6 +8,31 @@ import poodle from "../../assets/dog/poodle.png";
 export default function DetailsScreen() {
 
   const [chien, setChien] = useState("Votre chien");
+
+  const [age, seAge] = useState(0);
+  const [poids, setPoids] = useState(0);
+  const [couleur, setCouleur] = useState("");
+
+  useEffect(() => {
+    if (chien === "German Shepherds") {
+      seAge(3);
+      setPoids(30);
+      setCouleur("Noir et feu");
+    } else if (chien === "Chihuahuas") {
+      seAge(2);
+      setPoids(3);
+      setCouleur("Beige");
+    } else if (chien === "Poodles") {
+      seAge(4);
+      setPoids(10);
+      setCouleur("Blanc");
+    } else {
+      seAge(0);
+      setPoids(0);
+      setCouleur("");
+    }
+  }, [chien]);
+
 
   return (
     <ScrollView>
@@ -35,6 +60,19 @@ export default function DetailsScreen() {
         </View>
 
       </View>
+
+      <View/>
+
+      <Text style={styles.caca}> {"Detaille du Chien !!!"}</Text>
+      <Text style={styles.caca}>{"nom du chien sale:  "}{chien}</Text>
+      <Text style={styles.caca}>{"age du chien:  "}{age}{" ans"}</Text>
+      <Text style={styles.caca}>{"poids du chien:  "}{poids}{" kg"}</Text>
+      <Text style={styles.caca}>{"couleur du chien:  "}{couleur}</Text>
+      
+  
+      <View/>
+
+
     </ScrollView>
   );
 }
@@ -68,5 +106,12 @@ const styles = StyleSheet.create({
   }, 
   text: {
     textAlign: "center"
+  },
+  caca: {
+    left: 30,
+    height: 32,
+    fontSize: 22, 
+    fontWeight: '600', 
+    marginBottom: 8 
   }
 });
